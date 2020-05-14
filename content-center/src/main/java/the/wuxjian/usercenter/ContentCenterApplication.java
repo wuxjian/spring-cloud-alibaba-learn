a1f422506df5b2871d52ce2a945e5fcc61268391
wuxjian <wu_xjian@163.com>
2020-05-14 18:15:02 +0800

    初始化项目

---
+++ content-center/src/main/java/the/wuxjian/usercenter/ContentCenterApplication.java
@@ -0,0 +1,13 @@
+package the.wuxjian.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentCenterApplication.class, args);
+    }
+
+}
